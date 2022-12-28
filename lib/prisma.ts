2fd2942abe1197114bf042a1a8a2a7355a2675eb
TiveCS <ahmadfathanhanif@gmail.com>
2022-12-29 03:20:25 +0700

    change prisma library implementation

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,14 +1,10 @@
 import { PrismaClient } from '@prisma/client';
 
-let prisma: PrismaClient;
-
-if (process.env.NODE_ENV === 'production') {
-  prisma = new PrismaClient();
-} else {
-  if (!global.prisma) {
-    global.prisma = new PrismaClient();
-  }
-  prisma = global.prisma;
+declare global {
+  var prisma: PrismaClient | undefined;
 }
 
-export default prisma;
+const client = globalThis.prisma || new PrismaClient();
+if (process.env.NODE_ENV !== 'production') globalThis.prisma = client;
+
+export default client;
