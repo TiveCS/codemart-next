f0ff4b68c879c24943d5ce0c19394ef12fa4c230
TiveCS <ahmadfathanhanif@gmail.com>
2023-01-05 13:52:59 +0700

    change prisma lib

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -2,7 +2,7 @@ import { PrismaAdapter } from '@next-auth/prisma-adapter';
 import { AuthOptions } from 'next-auth';
 import NextAuth from 'next-auth/next';
 import GoogleProvider from 'next-auth/providers/google';
-import prisma from '../../../lib/prisma';
+import { prisma } from '../../../lib/prisma';
 
 export const authOptions: AuthOptions = {
   providers: [
