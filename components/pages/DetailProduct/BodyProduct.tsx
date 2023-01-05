44b99aa24b340ffc1a791c3420d18686dc8b1b0d
TiveCS <ahmadfathanhanif@gmail.com>
2023-01-05 13:53:28 +0700

    add image to product details

--- components/pages/DetailProduct/BodyProduct.tsx
+++ components/pages/DetailProduct/BodyProduct.tsx
@@ -1,3 +1,4 @@
+import Image from 'next/image';
 import { ProductWithAuthor } from '../../../pages/api/products/[id]';
 
 interface BodyProductProps {
@@ -9,6 +10,16 @@ const BodyProduct: React.FC<BodyProductProps> = ({
 }: BodyProductProps) => {
   return (
     <div>
+      <div className="flex items-center justify-center w-full mb-8">
+        <Image
+          src={product.image_url as string}
+          alt={product.title}
+          width={700}
+          height={500}
+          loading="lazy"
+        />
+      </div>
+
       <h4 className="mb-2 text-lg font-medium">Description</h4>
       <p className="text-base leading-loose">{product.description}</p>
     </div>
